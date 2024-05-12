979f34b1d465af4f5b0ea0b742504abe20e0d2f4
TamimSweedan <123229770+TamimSweedan@users.noreply.github.com>
2024-05-12 00:09:30 -0700

    Minor changes to these scripts
    Made some adjustments to these scripts

---
+++ EnemySpawner.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyToSpawn;
+    public float spawnTime = 3.0f;
+    public int spawnCount = 1;
+    float currentSpawnTime;
+    int currentSpawnCount;
+
+
+    void Start()
+    {
+        currentSpawnTime = spawnTime;
+        currentSpawnCount = spawnCount;
+    }
+
+
+    void Update()
+    {
+        if (currentSpawnCount == 0)
+        {
+            return;
+        }
+
+        currentSpawnTime -= Time.deltaTime;
+        if (currentSpawnTime < 0)
+        {
+            SpawnEnemy();
+            currentSpawnTime = spawnTime;
+            currentSpawnCount--;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+
+        GameObject newEnemy = Instantiate(enemyToSpawn, transform.position + Vector3.up * 0.501f, transform.GetChild(0).rotation);
+
+
+        if (!newEnemy.CompareTag("Target"))
+        {
+            newEnemy.tag = "Target";
+        }
+
+
+        CapsuleCollider capsuleCollider = newEnemy.GetComponent<CapsuleCollider>();
+        if (capsuleCollider == null)
+        {
+            capsuleCollider = newEnemy.AddComponent<CapsuleCollider>();
+            capsuleCollider.isTrigger = true; // 
+        }
+        else
+        {
+            capsuleCollider.isTrigger = true;
+        }
+
+
+        CapsuleHealth capsuleHealth = newEnemy.GetComponent<CapsuleHealth>();
+        if (capsuleHealth == null)
+        {
+            
+            capsuleHealth = newEnemy.AddComponent<CapsuleHealth>();
+        }
+
+        capsuleHealth.maxHealth = 100;
+    }
+}
